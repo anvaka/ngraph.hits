e85113a6052a86f451441997b943e1100b9a2630
Andriy Kashcha <anvaka@gmail.com>
2020-09-29 19:37:05 -0700

    fixed typos

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var fromDot = require('ngraph.fromdot');
 var hits = require('../');
 
 test('it calculates hubs and authorities for a graph', function(t) {
-  // This graph was comupted in the book http://www.mmds.org/ in Example 5.15.
+  // This graph was computed in the book http://www.mmds.org/ in Example 5.15.
   // Let's check our answers
   var graph = fromDot([
     'digraph G {',
@@ -14,19 +14,19 @@ test('it calculates hubs and authorities for a graph', function(t) {
     '}'].join('\n'));
 
   var result = hits(graph);
-  areClose(result.a.authority, 0.2087, 'a authoirty is correct');
+  areClose(result.a.authority, 0.2087, 'a authority is correct');
   areClose(result.a.hub, 1, 'a hub is correct');
 
-  areClose(result.b.authority, 1, 'b authoirty is correct');
+  areClose(result.b.authority, 1, 'b authority is correct');
   areClose(result.b.hub, 0.3583, 'b hub is correct');
 
-  areClose(result.c.authority, 1, 'c authoirty is correct');
+  areClose(result.c.authority, 1, 'c authority is correct');
   areClose(result.c.hub, 0, 'c hub is correct');
 
-  areClose(result.d.authority, 0.7913, 'd authoirty is correct');
+  areClose(result.d.authority, 0.7913, 'd authority is correct');
   areClose(result.d.hub, 0.7165, 'd hub is correct');
 
-  areClose(result.e.authority, 0, 'e authoirty is correct');
+  areClose(result.e.authority, 0, 'e authority is correct');
   areClose(result.e.hub, 0, 'e hub is correct');
   t.end();
 
